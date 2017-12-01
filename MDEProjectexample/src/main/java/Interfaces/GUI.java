290f972e974480ec31b0240838194bd9dce2e351
Zhanou Liang <gusliazh@student.gu.se>
2017-12-01 14:39:33 +0100

    add interfaces and gul

---
+++ MDEProjectexample/src/main/java/Interfaces/GUI.java
@@ -0,0 +1,20 @@
+package Interfaces;
+import java.util.Set;
+
+import project.AbstractRobotSimulator;
+import project.Point;
+
+import simbad.sim.EnvironmentDescription;
+public class GUI extends AbstractRobotSimulator {
+
+	public GUI( EnvironmentDescription e) {
+		super(null, null);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void update() {
+	
+		System.out.println("updating....");
+	}
+
+}
