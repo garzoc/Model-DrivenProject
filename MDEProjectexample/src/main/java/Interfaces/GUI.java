83bbb597ffbd111274dd468dce60997d5be73739
Zhanou Liang <gusliazh@student.gu.se>
2017-12-01 16:51:16 +0100

    fixing

--- MDEProjectexample/src/main/java/Interfaces/GUI.java
+++ MDEProjectexample/src/main/java/Interfaces/GUI.java
@@ -6,7 +6,7 @@ import project.AbstractSimulatorMonitor;
 import project.Point;
 import project.RobotAvatar;
 import simbad.sim.EnvironmentDescription;
-public class GUI extends AbstractSimulatorMonitor {
+public class GUI extends AbstractSimulatorMonitor implements Interface{
 
 	public GUI( Set<RobotAvatar> robots, EnvironmentDescription e) {
 		super(robots, e);
@@ -17,6 +17,12 @@ public class GUI extends AbstractSimulatorMonitor {
 	@Override
 	public void update(AbstractRobotSimulator arg0) {
 		// TODO Auto-generated method stub
+		
+	}
+
+
+	public void onRewardPointRecieved() {
+		
 		
 	}
 
