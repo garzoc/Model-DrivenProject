49bdc3b51f3dbe49e605fb3dbe94171c58ad7a65
carlosyechouh <carlos.yechouh@gmail.com>
2017-12-01 17:22:25 +0100

    minor changes

--- MDEProjectexample/src/main/java/rover/MissionController.java
+++ MDEProjectexample/src/main/java/rover/MissionController.java
@@ -1,18 +1,23 @@
-package project;
+package rover;
 
-public class Mission extends Thread{
+import project.Point;
+import project.RobotAvatar;
+import project.Strategies;
+
+public class MissionController extends Thread {
+
 	private final Strategies strategy;
 	private final RobotAvatar robot;
 	
-	Mission(Strategies strategy,RobotAvatar robot){
+	MissionController(Strategies strategy,RobotAvatar robot){
 		this.strategy=strategy;
 		this.robot=robot;
 	}
 	
 	
 	public void run() {
-		Point[] missionPoints=strategy.getPoints();
-		int missionProgress=0;
+		Point[] missionPoints = strategy.getPoints();
+		int missionProgress = 0;
 		while(missionProgress!=missionPoints.length) {
 			if(robot.isAtPosition(missionPoints[missionProgress])){
 				missionProgress++;
