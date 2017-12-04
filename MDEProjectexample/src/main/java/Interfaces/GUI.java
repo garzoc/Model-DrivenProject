a46a5c4c199e45eb299b05e90e94cbf920530801
Zhanou Liang <gusliazh@student.gu.se>
2017-12-04 12:14:29 +0100

    add mission1

--- MDEProjectexample/src/main/java/Interfaces/GUI.java
+++ MDEProjectexample/src/main/java/Interfaces/GUI.java
@@ -1,22 +1,36 @@
 package Interfaces;
+import java.awt.Point;
 import java.util.Set;
 
+import CentralStation.Singleton;
 import project.AbstractRobotSimulator;
 import project.AbstractSimulatorMonitor;
-import project.Point;
+
 import project.RobotAvatar;
+import rover.Strategies;
 import simbad.sim.EnvironmentDescription;
 public class GUI extends AbstractSimulatorMonitor implements Interface{
 
-	public GUI( Set<RobotAvatar> robots, EnvironmentDescription e) {
+	
+	public GUI( Set<RobotAvatar> robots, EnvironmentDescription e, Strategies plan) {
 		super(robots, e);
-		// TODO Auto-generated constructor stub
+		
+		Point[] pl1 = {new Point(3,-3), new Point(-3,-3), new Point(-7,-3)};
+//		Point[] pl2 =
+//		pl[2] = new Point(3,3);
+//		pl[3] = new Point(-3,3);
+		Strategies plan1 = new Strategies(pl1);
+		
+		Singleton.getCentralStation().getRobot(0).beginMission(plan1);
+		
+		
 	}
-
+	
 
 	@Override
 	public void update(AbstractRobotSimulator arg0) {
-		// TODO Auto-generated method stub
+		
+
 		
 	}
 
@@ -25,6 +39,7 @@ public class GUI extends AbstractSimulatorMonitor implements Interface{
 		
 		
 	}
+	
 	
 
 }
